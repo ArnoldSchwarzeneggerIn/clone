ac8bb4c1c8824da8c2e86de68666558fbc02c228
masebato <baradona69@gmail.com>
2016-11-08 17:20:54 -0500

    prueba 3

--- Prueba/Models/Persona.cs
+++ Prueba/Models/Persona.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Prueba.Conexions;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 
@@ -7,16 +9,27 @@ namespace Prueba.Models
 {
     public class Persona
     {
-        public string apellidos { get; set; }
-        public string nombres { get; set; }
-        public string genero { get; set; }
-        public string rh { get; set; }
-        public string correo { get; set; }
-        public string rol { get; set; }
-        public string departamento { get; set; }
-        public string municipio { get; set; }
-        public string estado { get; set; }
-        public string foto { get; set; }
+        Conexion conx = new Conexion();
+        Parametro[] para;
+        Transacion[] tran;
 
+        public string nombres { get; set; }
+        public string apellidos { get; set; }
+      
+        public string Pers_id { get; set; }
+        public string Identificacion { get; set; }
+        public string tipousuario { get; set; }
+        public string programa { get; set; }
+        //public string departamento { get; set; }
+        //public string municipio { get; set; }
+        //public string estado { get; set; }
+        //public string foto { get; set; }
+
+
+        public DataTable ConsultarPersonaJurado()
+        {
+            return conx.realizarConsulta("PR_CNST_JURAPERSONA", "CR_CNST_JURAPERSONA", para);
+
+        }
     }
 }
